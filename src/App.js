73fc54da5a51e3f13b1217282a8cac07d0937ded
DevMiller__ <solomondev002@gmail.com>
2022-01-08 05:06:27 -0800

    coming soon page

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontFamily: "poppins",
+      }}
+    >
+      <h1
+        style={{
+          margin: 0,
+          color: "#6EC177",
+        }}
+      >
+        Hiya! Dev Miller is building something hoooge, stay tuned!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
